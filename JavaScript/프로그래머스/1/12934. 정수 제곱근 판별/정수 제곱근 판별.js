d6971399f5693d72d341d1be7db99acd3ed1da81
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-07-14 13:54:47 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ JavaScript/프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,7 +1,13 @@
 function solution(n) {
     // console.log(Math.sqrt(n));
     
-    if(Number.isInteger(Math.sqrt(n))) return (Math.sqrt(n) + 1) ** 2; 
-    else return -1;
+    // if(Number.isInteger(Math.sqrt(n))) return (Math.sqrt(n) + 1) ** 2; 
+    // else return -1;
+    
+    const sqrt = Math.sqrt(n);
+    if(!(n % sqrt))
+        return (sqrt + 1) ** 2;
+    
+    return -1;
     
 }
