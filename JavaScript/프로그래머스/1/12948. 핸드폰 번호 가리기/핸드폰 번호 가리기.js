06afa5d65fccf2a3ee0d8e8b6e73710a09705997
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-07-14 14:31:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ JavaScript/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,8 @@
-function solution(phone_number) {
-    // console.log(phone_number.slice(0, -4));
-    const number = phone_number.slice(0, -4);
-    // return phone_number.replace(phone_number.slice(0, -4), '*'.repeat(phone_number.slice(0, -4);.length));
-      return phone_number.replace(number, '*'.repeat(number.length));
-}
+// function solution(phone_number) {
+//     // console.log(phone_number.slice(0, -4));
+//     const number = phone_number.slice(0, -4);
+//     // return phone_number.replace(phone_number.slice(0, -4), '*'.repeat(phone_number.slice(0, -4);.length));
+//       return phone_number.replace(number, '*'.repeat(number.length));
+// }
+
+const solution = str => `*`.repeat(str.length - 4) + str.substr(-4);
