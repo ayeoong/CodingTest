a5214b6e7704c16d878ac1c51fbbd604a3694435
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-07-14 11:04:58 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -0,0 +1,24 @@
+const solution = s => {
+    // const regex = /\d{4}$/;
+    // const regex2 = /\d{6}$/;
+    
+    return /^\d{4}$|^\d{6}$/.test(s);
+    // if(regex.test(s) || regex2.test(s)) return true;
+    // else return false;
+    // console.log(regex.test(s));
+    // console.log(regex2.test(s));
+    
+    
+    // if(s.length === 4 || s.length === 6)   
+    //     return [...s].every(v => !isNaN(v));
+    
+    // 전부 true면 숫자로만 된 거 ......?
+    // [...s].filter(v => console.log(Number(v)));
+    // var answer = true;
+    // return answer;
+    
+    // for(let i=0; i<s.length; i++) {
+    //     if(s.length === 4 || s.length === 6) console.log(s[i]);
+    //     else false;
+    // }
+}
