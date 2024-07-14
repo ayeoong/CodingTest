86f2927ce1fd7d1bde6f78770b349fcf76d2c45a
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-07-14 13:49:25 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ JavaScript/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -1,5 +1,1 @@
-function solution(n) {
-    // console.log([...n.toString()].reverse().map(v => +v));
-    // return [...n.toString()].map(v => +v).sort((a, b) => b - a);
-    return [...n.toString()].reverse().map(v => +v);
-}
+const solution = n => [...n.toString()].reverse().map(Number);
