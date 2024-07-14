27f4c61307af1053af598babef0c323ac2660d68
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-07-14 14:06:28 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 6.07 ms, Memory: 36.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ JavaScript/프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,16 +1,14 @@
 function solution(n) {
     
-    for (let i=0; i<n; i++){
-        if (n%i == 1){
-            return i;
-        }
-    }
-    // let x = 0;
-    // while(true) {
-    //     if(n % x === 1) {
-    //         break;
+    // for (let i=0; i<n; i++){
+    //     if (n%i == 1){
+    //         return i;
     //     }
-    //     x++;
     // }
-    // return x;
+    
+    let x = 0;
+    while(true) {
+        x++;
+        if(n % x === 1) return x;
+    }
 }
