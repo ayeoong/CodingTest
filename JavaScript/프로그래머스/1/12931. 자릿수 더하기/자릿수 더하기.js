bac24532531507d2e7ee40fa636a49e69cc686ab
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-07-14 13:47:56 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
+++ JavaScript/프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,1 @@
-function solution(n)
-{
-    const arr = [...n.toString()].map(v => +v);
-    return arr.reduce((a, c) => a += c, 0);
-    
-    // let num = 0;
-    // [...n.toString()].reduce((a, c) => parseInt(a) += +c, 0);
-    // return 1;
-}
+const solution = n => [...n.toString()].map(v => +v).reduce((a, c) => a += c, 0);
