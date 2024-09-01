67b13b44b2c9bc5b5d9c9a2f8c14b7ee69aeba2a
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-09-01 17:21:35 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
+++ JavaScript/프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,19 +1,33 @@
-// 12:08 ~ 12:20
-function solution(array, commands) {
-    let answer = [];
-    for (let i = 0; i < commands.length; i++) {
-        // console.log(commands[i]);
-        // console.log(commands[i][0]);
-        // console.log(commands[i][0] - 1); // -1 해줘야 n번째 가능 idx는 0부터 시작
-        let arr = array.slice(commands[i][0] - 1, commands[i][1]);
-        
-        arr.sort((a, b) => a - b);
-        
-        // console.log(arr[commands[i][2]]);
-        
-        
-        answer.push(arr[commands[i][2] - 1]);
-    }
-    return answer;
+const solution = (array, commands) => commands.map(([x, y, z]) => array.slice(x - 1, y).sort((a, b) => a - b)[z - 1]);
 
-}
+
+
+
+
+// const solution = (array, commands) => commands.map(([x, y, z]) => {
+//     // console.log(x, y, z)
+//     console.log(array.slice(x - 1, y).sort((a, b) => a - b)[z - 1]);
+//     // array.slice(x - 1, y).sort((a, b) => a - b)
+// })
+
+
+
+
+
+
+// 12:08 ~ 12:20
+// function solution(array, commands) {
+//     let answer = [];
+//     for (let i = 0; i < commands.length; i++) {
+//         let arr = array.slice(commands[i][0] - 1, commands[i][1]);
+        
+//         arr.sort((a, b) => a - b);
+//         answer.push(arr[commands[i][2] - 1]);
+//     }
+//     return answer;
+
+// }
+
+// console.log(commands[i]);
+// console.log(commands[i][0]);
+// console.log(commands[i][0] - 1); // -1 해줘야 n번째 가능 idx는 0부터 시작
