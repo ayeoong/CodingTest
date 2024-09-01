ebc4635ed3db07df1be74c6d44ed3957332e3e79
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-09-01 13:12:39 +0900

    [level 1] Title: 2016년, Time: 11.69 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,6 @@
+function solution(a, b) {
+    // var answer = '';
+    // return answer;
+    // console.log(new Date(2016, a - 1, b).toString().slice(0, 3).toUpperCase());
+    return new Date(2016, a - 1, b).toString().slice(0, 3).toUpperCase();
+}
