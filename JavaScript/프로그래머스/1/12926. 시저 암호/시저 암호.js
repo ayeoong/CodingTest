7c1b979fa5ed815ab36a15253aa26f5a369a8f10
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-09-01 16:39:36 +0900

    [level 1] Title: 시저 암호, Time: 1.64 ms, Memory: 36.9 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ JavaScript/프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,34 +1,40 @@
 function solution(s, n) {
+    return [...s].map(v => {
+        // console.log(v);
+        if(v !== ` `) {
+            const next = v.charCodeAt() + n;
+            // console.log(String.fromCharCode(next));
+            // a = 97, A = 65 // a - z까지 25만큼 차이
+            v = String.fromCharCode(v.toLowerCase().charCodeAt() + n > 122? next - 26 : next)
+        }
+        
+        return v;
+        
+    }).join(``);
     
     // console.log(s.charCodeAt());
-    let answer = "";
-    [...s].forEach(v => {
-         if(v === ` `) answer += ` `;
-         else if(v >= `A` && v <= `Z`) {
-            // 대문자 처리
-            // console.log(v.charCodeAt() + n);
-            // console.log(String.fromCharCode(change));
-            // const change = String.fromCharCode(v.charCodeAt() + n);
+//     let answer = ``;
+//     [...s].forEach(v => {
+//          if(v === ` `) answer += ` `;
+//          else if(v >= `A` && v <= `Z`) {
+//             // 대문자 처리
+//              const ascii = v.charCodeAt() + n;
+//              if(ascii > 90) {
+//                  answer += String.fromCharCode(ascii - 26);
+//              } else answer += String.fromCharCode(ascii);
              
-             const ascii = v.charCodeAt() + n;
-             if(ascii > 90) {
-                 answer += String.fromCharCode(ascii - 26);
-             } else answer += String.fromCharCode(ascii);
-             // return String.fromCharCode(change);
-         } else if(v >= `a` && v <= `z`) {
-            // 소문자 처리
-             const ascii = v.charCodeAt() + n;
+//          } else if(v >= `a` && v <= `z`) {
+//             // 소문자 처리
+//              const ascii = v.charCodeAt() + n;
+//               if(ascii > 122) {
+//                   answer += String.fromCharCode(ascii - 26);
+//              } else answer += String.fromCharCode(ascii);
              
-              if(ascii > 122) {
-                  answer += String.fromCharCode(ascii - 26);
-             } else answer += String.fromCharCode(ascii);
-             
-         }
-    });
+//          }
+//     });
     
-    return answer;
+//     return answer;
 }
-
 
 // a = 97 A = 65
 // s를 n만큼 밀어서 알파벳 이동
