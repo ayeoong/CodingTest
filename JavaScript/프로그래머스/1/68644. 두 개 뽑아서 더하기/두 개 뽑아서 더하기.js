0140cb342bcfec879fe81d118b8442d825534fad
Skye Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-09-01 17:36:07 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,28 @@
+function solution(numbers) {
+    let data = {};
+    
+    for(let i=0; i<numbers.length; i++){
+        for(let j=i+1; j<numbers.length; j++){
+            // answer.push(numbers[i] + numbers[j]);
+            const sum = numbers[i] + numbers[j];
+            // console.log(data, sum, data[sum]);
+            if(!data[sum])
+                data[sum] = true;
+            
+        }  
+    }
+    
+    return Object.keys(data).map(Number);
+    // console.log(data)
+    // console.log(Object.keys(data).map(Number))
+//     let answer = [];
+    
+//     for(let i=0; i<numbers.length; i++){
+//         for(let j=i+1; j<numbers.length; j++){
+//             answer.push(numbers[i] + numbers[j]);
+//         }  
+//     }
+//    // return answer.sort((a, b) => a - b);
+//      return [...new Set(answer)].sort((a, b) => a - b);
+    
+}
