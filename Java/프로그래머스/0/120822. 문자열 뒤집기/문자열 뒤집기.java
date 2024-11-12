7e3cb493338ff5344ab8f83c39d1386a449236df
Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-11-12 11:08:09 +0900

    [level 0] Title: 문자열 뒤집기, Time: 14.95 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        
+        String reverse = "";
+        for(int i=my_string.length() - 1; i >= 0; i--) {
+            reverse += my_string.charAt(i);
+        }
+        
+        return reverse;
+    }
+}
