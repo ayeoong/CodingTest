f77c03d0d619e4c0c3599f5772fbc937aa2219af
Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2024-12-17 08:23:47 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- Java/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ Java/프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,17 +1,10 @@
-import java.util.*;
-
 class Solution {
-    public List<Integer> solution(int[] numbers) {
-        int[] t1 = { 1, 2, 3, 4 };
-        List<Integer> t2 = new ArrayList<Integer>();
+    public int[] solution(int[] numbers) {
         
-        System.out.println(t2);
         for(int i=0; i<numbers.length; i++) {
-            // System.out.println(numbers[i] * 2);
-            t2.add(numbers[i] * 2);
-            
+            numbers[i] *= 2;
         }
         
-        return t2;
+        return numbers;
     }
 }
