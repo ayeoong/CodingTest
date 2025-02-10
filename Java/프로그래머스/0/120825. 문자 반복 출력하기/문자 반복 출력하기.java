0c1dc2bd81bb7aa2ee0780d88a10035af67b850b
Ayeong Kim <100174084+ayeoong@users.noreply.github.com>
2025-02-11 08:14:38 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.76 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        String[] str = my_string.split("");
+        
+        for(int i=0; i<str.length; i++) {
+            // System.out.println(str[i]);
+            answer += str[i].repeat(n);
+        }
+        
+        return answer;
+    }
+}
